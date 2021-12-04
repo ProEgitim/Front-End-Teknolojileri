74537aee039d61889d4e488cc3e1bfd553b55a73
bugrabolat <65684263+bugrabolat@users.noreply.github.com>
2021-12-04 14:06:19 +0300

    js giris

---
+++ Ogrenciler/Buğra/js/ders01/script.js
@@ -0,0 +1,70 @@
+//var firstnumber =20;
+//var secondnumber = 20;
+
+//console.log(firstnumber+secondnumber);
+
+//boolean
+//var a = true;
+//console.log(a);
+
+//null
+//var a = null;
+//console.log(a);
+//console.log(typeof a);
+
+//object
+//var user={name:"bugra bolat",age:20};
+//console.log(user);
+//console.log(typeof user);
+//console.log(user.name);
+//console.log(user.age);
+
+//array:diziler
+// var users= ["can","kemal",30];
+// console.log(users);
+// console.log(typeof users);
+// console.log(users[1]);
+
+// var date = new Date();
+// console.log(date);
+// console.log(typeof date);
+
+//scope
+
+// var a = 10;
+// let b = 20;
+// const c = 30;
+
+// {
+//     const d ="merhaba";
+//     console.log(d);
+// };
+// console.log(d);
+
+//tip donusturme
+
+// let value;
+
+// value = String(123);
+// value = String(3.14);
+// value = String(false);
+
+
+// console.log(value);
+// console.log(typeof value);
+
+let value;
+const firstname = "bugra";
+const lastname = "bolat";
+const department = "bilisim";
+const salary = "3750";
+
+value = firstname + lastname;
+value = firstname + " " +lastname;
+value = firstname;
+value +=" "+lastname;
+
+
+
+
+
