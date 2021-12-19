c3b92e7479ae73566fe11647aa3f24fe84cf9478
Varol Maksutoğlu <varolmaksutoglu@yandex.com>
2021-12-19 13:45:25 +0300

    bilet

--- Dersler/js/Ders05-ES6/Referanslar/script.js
+++ Dersler/js/Ders05-ES6/Referanslar/script.js
@@ -1,19 +1,19 @@
 // Referasnlar
 
-// let a = "Deneme";
+// let a = "Deneme"; // Value
 // let b = "Deneme";
 
-// let c = ["a","b"];
+// let c = ["a","b"]; // Referans
 // let d = ["a","b"];
 
 // let e = 1;
-// let f = 2;
+// let f = "1";
 
-// console.log("String Değişken", a===b); // osman, hira = true ---- sercan = false
+// console.log("String Değişken", a===b); // osman, hira = true ---- sercan = false // TRUE
 
-// console.log("Array", c===d); // herkes=true
+// console.log("Array", c===d); // herkes=true // FALSE
 
-// console.log("Number", e===f);
+// console.log("Number", e===f); // FALSE
 
 // let a = "10";
 // let b = 20;
@@ -24,7 +24,7 @@
 // }
 
 // if (c(a,b)){
-//   var f = c(a,b)
+//   let f = c(a,b);
 // }
 // console.log(f);
 
