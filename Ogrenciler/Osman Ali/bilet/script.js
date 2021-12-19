3bfab5a9d8d7bd097353a77241c9cdd51925bc3b
osmanalikacar <bilgi@benosso.com>
2021-12-19 11:55:26 +0300

    Ticket
    commit ticket

---
+++ Ogrenciler/Osman Ali/bilet/script.js
@@ -0,0 +1,41 @@
+const tumKoltuklar = document.querySelectorAll(".seat");
+const seats = document.querySelectorAll(".seat:not(.reserved;)");
+const container = document.querySelector(".container");
+const count = document.getElementById("count");
+const amount = document.getElementById("amount");
+const movie = document.getElementById("movie");
+
+eventListeners();
+function eventListeners() {
+    container.addEventListener("click", selectSeat);
+}
+
+function selectSeat(e) {
+    if (e.target.classList.contains("seat") && !e.target.classList.contains("reserved")){
+        e.target.classList.toggle("selected");
+        calculateTotal();
+    }
+}
+
+function calculateTotal() {
+    const selectedSeats = container.querySelectorAll(".seat.selected");
+    const selectedSeatsArr = [];
+    const seatsArr = [];
+    selectedSeats.forEach(function(seat) {
+        selectedSeatsArr.push(seat);
+    });
+    seats.forEach(function(seat) {
+        seatsArr.push(seat);
+    });
+    let selectedSeatIndexs = selectedSeatsArr.map(function(seat) {
+        return seatsArr.indexOf(seat);
+    });
+    let selectedSeatsCount = selectedSeats.length;
+    count.innerText = selectedSeatsCount;
+    amount.innerText = selectedSeatsCount * movie.value ;
+    addLocalStorage(selectedSeatIndexs);
+}
+
+function addLocalStorage(selectedSeatIndexs){
+    localStorage.setItem("selectedIndex", JSON.stringify(selectedSeatIndexs));
+}
