5cc4c910d70c37fa4719cf5f9ee5815c89dc90c1
hiranurozkanhoruzoglu <79055283+hiranurozkanhoruzoglu@users.noreply.github.com>
2021-12-19 09:04:50 +0300

    es-6

---
+++ Ogrenciler/Hiranur-Ozkan-Horuzoglu/js/Ders05-ES6/Referanslar/script.js
@@ -0,0 +1,30 @@
+// Referasnlar
+
+// let a = "Deneme";
+// let b = "Deneme";
+
+// let c = ["a","b"];
+// let d = ["a","b"];
+
+// let e = 1;
+// let f = 2;
+
+// console.log("String Değişken", a===b); // osman, hira = true ---- sercan = false
+
+// console.log("Array", c===d); // herkes=true
+
+// console.log("Number", e===f);
+
+// let a = "10";
+// let b = 20;
+
+// const c = (a,b) => {
+//   let e = Number(a);
+//   return e+b;
+// }
+
+// if (c(a,b)){
+//   var f = c(a,b)
+// }
+// console.log(f);
+
---
+++ Ogrenciler/Hiranur-Ozkan-Horuzoglu/js/Ders05-ES6/SpreadOperator/script.js
@@ -0,0 +1,17 @@
+// const langs = ["Python", "C++", "Go", "Ruby"];
+
+// // console.log(langs[0],langs[1],langs[2],langs[3]);
+// console.log(...langs);
+
+// const langs2 = ["Javasript","C#",...langs];
+
+// console.log("listeler:",...langs2);
+
+// const langs = ["Python", "C++", "Go", "Ruby"];
+// const langs2 = ["JS", "C#", "Pascal", "Rust"];
+// const langs3 = ["Php", "Java", "Assembly", "QBasic"];
+
+// const allLangs = [...langs,...langs2,...langs3];
+
+// console.log(allLangs);
+
