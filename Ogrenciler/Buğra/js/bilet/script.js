ed6405bc20156e74c3402591731dd6653f53f0a9
bugrabolat <65684263+bugrabolat@users.noreply.github.com>
2021-12-23 22:39:10 +0300

    js

--- Ogrenciler/Buğra/js/bilet/script.js
+++ Ogrenciler/Buğra/js/bilet/script.js
@@ -3,37 +3,63 @@ const count = document.getElementById('count');
 const amount = document.getElementById('amount');
 const select = document.getElementById('movie');
 
-
+let reserveds;
 
 const container = document.querySelector(".container");
-container.addEventListener("click",koltukSec);
+container.addEventListener("click", koltukSec);
 
-function koltukSec (e) {
+function koltukSec(e) {
 
-    if (e.target.classList.contains ('seat') && !e.target.classList.contains('reserved')){
-        e.target.classList.toggle('selected'); 
+    if (e.target.classList.contains('seat') && !e.target.classList.contains('reserved')) {
+        e.target.classList.toggle('selected');
+        e.target.className = "seat selected";
+    } else if (e.target.classList.contains("seat") && (!e.target.classList.contains("reserved")) && (e.target.classList.contains("selected"))) {
+        e.target.className = "seat";
     }
     toplamHesap();
-    
+
 }
-select.addEventListener('change', function(e){
-    toplamHesap();
-})
+select.addEventListener('change', function (e) {
+    console.log(select.selectedIndex);
+    // toplamHesap();
+    temizle();
+});
+
+function temizle() {
+    document.querySelectorAll(".container .selected").forEach(function(element) {element.classList.remove('selected')});
+}
+
+//  const secilikoltuklar = [
+//      {
+//          movieName:"movie1",
+//          selectedSeats:[1,2,3]
+//      },
+//      {
+//          movieName:"movie2",
+//          selectedSeats:[4,5,6]
+//      },
+//      {
+//          movieName:"movie3",
+//          selectedSeats:[7,8,9]
+//      }
+//  ]
+
+function toplamHesap() {
+    let selectedSeats = container.querySelectorAll('.seat.selected');
+    const selectedSeatsArr = [...selectedSeats];
+    const seatsArr = [...seat];
+    let selectedSeatIndexArr = selectedSeatsArr.map(function (i) {
+        return seatsArr.indexOf(i)
+    });
+    let selectedSeatCount = selectedSeats.length;
+    count.innerText = selectedSeatCount;
+    amount.innerText = selectedSeatCount * select.value;
+    saveToLocalStorage(selectedSeatIndexArr);
+
+}
+
+function saveToLocalStorage(index) {
+    localStorage.setItem('selectedSeats' + select.selectedIndex, JSON.stringify(index));
 
 
-function toplamHesap(){
-        let selectedSeats = container.querySelectorAll('.seat.selected');
-        const selectedSeatsArr = [...selectedSeats];
-        const seatsArr = [...seat];
-  let selectedSeatIndexArr = selectedSeatsArr.map(function(i){return seatsArr.indexOf(i)});
-        let selectedSeatCount = selectedSeats.length;      
-        count.innerText = selectedSeatCount;    
-        amount.innerText = selectedSeatCount * select.value;
-        saveToLocalStorage (selectedSeatIndexArr);
-    
 }
-    function saveToLocalStorage (index) {
-        localStorage.setItem('selectedSeats',JSON.stringify(index));
-        
-    }
-  
