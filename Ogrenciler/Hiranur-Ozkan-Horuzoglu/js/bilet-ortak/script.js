e85f96344b2748e1d9636cf181ddbe7d80d2a93a
hiranurozkanhoruzoglu <79055283+hiranurozkanhoruzoglu@users.noreply.github.com>
2021-12-24 22:19:49 +0300

    sinema

--- Ogrenciler/Hiranur-Ozkan-Horuzoglu/js/bilet-ortak/script.js
+++ Ogrenciler/Hiranur-Ozkan-Horuzoglu/js/bilet-ortak/script.js
@@ -12,13 +12,11 @@ function koltukSec(e) {
 
     if (e.target.classList.contains('seat') && !e.target.classList.contains('reserved')) {
         e.target.classList.toggle('selected');
-        e.target.className = "seat selected";
     } else if (e.target.classList.contains("seat") && (!e.target.classList.contains("reserved")) && (e.target.classList.contains("selected"))) {
         e.target.className = "seat";
     }
-    let selectedSeatIndexArr = toplamHesap();
-    saveToLocalStorage(selectedSeatIndexArr);
-
+    toplamHesap();
+    saveToLocalStorage();
 }
 
 select.addEventListener('change', function (e) {
@@ -28,21 +26,19 @@ select.addEventListener('change', function (e) {
 });
 
 function temizle() {
-    document.querySelectorAll(".container .selected").forEach(function(element) {element.classList.remove('selected')});
+    document.querySelectorAll(".container .selected").forEach(function(element) {
+        element.classList.remove('selected');
+    });
+    document.querySelectorAll(".container .reserved").forEach(function(element) {
+        element.classList.remove('reserved');
+    });
 }
 
 function toplamHesap() {
     let selectedSeats = document.querySelectorAll('.container .seat.selected');
-    const selectedSeatsArr = [...selectedSeats];
-    const seatsArr = [...seats];
-    let selectedSeatIndexArr = selectedSeatsArr.map(function (i) {
-        return seatsArr.indexOf(i)
-    });
     let selectedSeatCount = selectedSeats.length;
     count.innerText = selectedSeatCount;
     amount.innerText = selectedSeatCount * select.value;
-   
-    return selectedSeatIndexArr;
 }
 
 function getFromLocalStorage() {
@@ -51,12 +47,20 @@ function getFromLocalStorage() {
     if (selectedSeats !=null && selectedSeats.length > 0) {
         seats.forEach(function(seat, index) {
             if (selectedSeats.indexOf(index) > -1) {
-                seat.classList.add('selected');
+                seat.classList.add('reserved');
             }
         });
     }
 }
 
-function saveToLocalStorage(index) {
-    localStorage.setItem('selectedSeats' + select.selectedIndex, JSON.stringify(index));
+function saveToLocalStorage() {
+    let selectedSeats = document.querySelectorAll('.container .seat.selected');
+    let reserverdSeats = document.querySelectorAll('.container .seat.reserved');
+    const selectedAndReservedSeatsArr = [...selectedSeats, ...reserverdSeats];
+    const seatsArr = [...seats];
+    let selectedSeatIndexArr = selectedAndReservedSeatsArr.map(function (i) {
+        return seatsArr.indexOf(i)
+    });
+
+    localStorage.setItem('selectedSeats' + select.selectedIndex, JSON.stringify(selectedSeatIndexArr));
 }
