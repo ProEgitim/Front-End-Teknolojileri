686d78acb8dd303717574a8844ea798f48bbd958
osmanalikacar <bilgi@benosso.com>
2021-12-25 11:47:15 +0300

    M..
    ds

---
+++ Ogrenciler/OsmanAli/Dersler/js/Ders05-ES6/Referanslar/script.js
@@ -0,0 +1,30 @@
+// Referasnlar
+
+// let a = "Deneme"; // Value
+// let b = "Deneme";
+
+// let c = ["a","b"]; // Referans
+// let d = ["a","b"];
+
+// let e = 1;
+// let f = "1";
+
+// console.log("String Değişken", a===b); // osman, hira = true ---- sercan = false // TRUE
+
+// console.log("Array", c===d); // herkes=true // FALSE
+
+// console.log("Number", e===f); // FALSE
+
+// let a = "10";
+// let b = 20;
+
+// const c = (a,b) => {
+//   let e = Number(a);
+//   return e+b;
+// }
+
+// if (c(a,b)){
+//   let f = c(a,b);
+// }
+// console.log(f);
+
