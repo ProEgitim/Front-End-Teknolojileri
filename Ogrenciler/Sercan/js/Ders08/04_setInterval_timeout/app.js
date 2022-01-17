e3d0f113850b64220a123306997b418bdbe5e8d4
sercan masar <smasar@outlook.com>
2022-01-17 23:41:44 +0300

    studies in lesson

---
+++ Ogrenciler/Sercan/js/Ders08/04_setInterval_timeout/app.js
@@ -0,0 +1,17 @@
+// setTimeout(function(){
+//   console.log("selam");
+// },5000);
+
+
+
+// ////  Set interval
+
+// let i = 0;
+// let value= setInterval(() => {
+//   i++;
+//     console.log("sayı", i);
+//  }, 1000);
+
+//  document.getElementById("btn").addEventListener("click", function(){
+//    clearInterval(value);
+//  });
