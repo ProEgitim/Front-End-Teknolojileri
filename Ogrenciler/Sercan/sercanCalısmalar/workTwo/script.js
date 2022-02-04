fee39f72a12e7b941ac07a501a3127e2d4fac0d0
sercan masar <smasar@outlook.com>
2022-02-04 22:58:43 +0300

    working on functions
    -made a fizzbuzz function,
    -working about for loop,while loop, if else in css

--- Ogrenciler/Sercan/sercanCalısmalar/workTwo/script.js
+++ Ogrenciler/Sercan/sercanCalısmalar/workTwo/script.js
@@ -3,12 +3,7 @@ let card = document.querySelector(".card-sc");
 document.querySelector(".parsePhoto").addEventListener("mouseover", big);
 
 function big(e){ 
-
-  console.log(e.target)
-  if(e.target.matches('.img')){
-   e.target.childNodes[1].childNodes[1].classList.add('big'); 
-  }
- 
+  e.target.style='color:rgba(0, 0, 0, 0.3)' ;
 }
 
 
