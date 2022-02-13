c19ef85aab067a9dde61c905a349dcc026703b08
osmanalikacar <bilgi@benosso.com>
2022-02-13 14:17:25 +0300

    React
    React

---
+++ Ogrenciler/OsmanAli/Dersler/React/01-Giris/deneme/src/Title.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Title extends Component {
+  render() {
+    return (
+      <div>
+      <h1>Satılık Domain Listesi</h1>
+      </div>
+    )
+  }
+}
