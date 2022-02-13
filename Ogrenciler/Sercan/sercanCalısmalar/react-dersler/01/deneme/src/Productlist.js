c1d362cf5e2edad5d651231c77df272d0e9d0416
sercan masar <smasar@outlook.com>
2022-02-14 00:07:40 +0300

    working on typescript

---
+++ Ogrenciler/Sercan/sercanCalısmalar/react-dersler/01/deneme/src/Productlist.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Productlist extends Component {
+  render() {
+    return (
+      <div>
+          <p>product list</p>
+      </div>
+    )
+  }
+}
