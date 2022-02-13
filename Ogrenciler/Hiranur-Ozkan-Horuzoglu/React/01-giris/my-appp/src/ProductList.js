be76d8285a64a8f7139f47c7a35f3209b1dd0947
hiranurozkanhoruzoglu <79055283+hiranurozkanhoruzoglu@users.noreply.github.com>
2022-02-13 13:42:53 +0300

    js-react

---
+++ Ogrenciler/Hiranur-Ozkan-Horuzoglu/React/01-giris/my-appp/src/ProductList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ProductList  extends Component {
+  render() {
+    return (
+      <div>
+        <p>ProductList Compenent</p>
+      </div>
+    )
+  }
+}
