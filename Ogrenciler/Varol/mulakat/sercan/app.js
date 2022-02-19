ed2f6d1cdd890cc8270c06c0a0726c50d150f25f
Varol Maksutoğlu <varolmaksutoglu@yandex.com>
2022-02-19 14:03:07 +0300

    sercan

---
+++ Ogrenciler/Varol/mulakat/sercan/app.js
@@ -0,0 +1,14 @@
+// var ile let arasındaki fark nedir?
+// undefined ve null arasındaki fark nedir?
+// == ve === arasında ne fark vardır?
+
+// console.log('one');
+// setTimeout(function() {
+//   console.log('two');
+// }, 0);
+// console.log('three');
+
+// username = "Sercan";
+// var username; 
+// var username; 
+// console.log(username);
