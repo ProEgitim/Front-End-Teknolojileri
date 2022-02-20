49ac63662b47511648cde88da8d8d131ba67e2a7
sercan masar <smasar@outlook.com>
2022-02-20 13:25:35 +0300

    last state

--- Ogrenciler/Sercan/js/react/03-Proje-React/react-blog/src/components/Headers.js
+++ Ogrenciler/Sercan/js/react/03-Proje-React/react-blog/src/components/Headers.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function Headers() {
   return (
-    <h1>Front End Blogs</h1>
+    <h1>Front End Blogs 
+ 
+    </h1>
   )
 }
--- Ogrenciler/Sercan/js/react/03-Proje-React/react-blog/src/components/List.js
+++ Ogrenciler/Sercan/js/react/03-Proje-React/react-blog/src/components/List.js
@@ -3,16 +3,16 @@ import React from 'react'
 const List = (props)=> {
     const blogs = props.blogs;
   return (
-    <div>{
-        blogs.map((blog)=>(
+    <div>
+       { blogs.map((blog)=>(
             <div key={blog.id}>
                 <h2>{blog.title}</h2>
                 <p>{blog.content}</p>
                 <p>Yazar: {blog.yazar}</p>
                 <button onClick={()=> props.onHandle(blog.id)}>Delete</button>
             </div>
-        ))
-    }</div>
+        )) }
+    </div>
   )
 }
 
