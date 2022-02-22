19d5b0643e2b425bb3a284ba1a80ac2960447595
sercan masar <smasar@outlook.com>
2022-02-22 23:42:01 +0300

    compiling code in files

--- Ogrenciler/Sercan/sercanCalısmalar/react-calısmalar/02-Deneme/deneme-app/src/components/Main.jsx
+++ Ogrenciler/Sercan/sercanCalısmalar/react-calısmalar/02-Deneme/deneme-app/src/components/Main.jsx
@@ -2,58 +2,23 @@ import React from 'react'
 
 const Main = (props)=> {
     let blogs = props.blogs;
-    const styledDiv = {
-        backgroundColor: '#f3f3f3',
-        display : 'flex',
-        flexDirection:'row',
-        width:'70%',
-        margin : '5px 5px',
-        alignItems:'center',
-        justifyContent: 'space-between',
-        
-
-    }
-    const outerDiv = {
-        backgroundColor: '#f3f3f3', 
-        display : 'flex',
-        flexDirection:'column',
-        width:'70%',
-        margin : '5px 5px',
-        alignItems:'center'  
-    }
-
-    const itemDiv ={
-       border:' 1px solid ',
-       width: '200px',
-       height:'200px',
-       boxSizing:'border-box',
-       margin: '5px 5px',
-       backgroundColor:'#f9f5f6',
-       boxShadow : '6px 5px 0 0 #aaaaaa'
-    }
-
-   const styleDelete ={
-        margin: '20px 20px',
-        padding: '7px',
-        backgroundColor:'#aaaaaa',
-        color:'#fff'
-    }
+    
   return (
-      <div style={outerDiv}>
+      <div className='outerDiv'>
     <h2>Blogs About Javascript Libraries</h2>
-    <div style={styledDiv}>
+    <div className='styledDiv'>
     
     {blogs.map((blog)=>(
-        <div style={itemDiv} key={blog.id}>
+        <div className='itemDiv' key={blog.id}>
         <h3>{blog.topic}</h3>
         <div>Issue:{blog.issue}</div>
         <div>ReadTime:{blog.readTime}</div>
         <div>Author:{blog.author}</div>
-        <button style={styleDelete} onClick={()=> props.handleDelete(blog.id)}>Delete</button>
+        <button className='styleDelete' onClick={()=> props.handleDelete(blog.id)}>Delete</button>
         </div>
     ))}</div>
 
-    
+
     </div>
   )
 }
