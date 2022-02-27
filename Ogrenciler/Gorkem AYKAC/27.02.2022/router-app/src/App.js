cf55fb78efe6d4e2bc4581270ec118df95ace24f
grkemayk <grkemaykac1997@hotmail.com>
2022-02-27 12:25:08 +0300

    Home Page

--- Ogrenciler/Gorkem AYKAC/27.02.2022/router-app/src/App.js
+++ Ogrenciler/Gorkem AYKAC/27.02.2022/router-app/src/App.js
@@ -1,38 +1,43 @@
 import { Routes, Route, Link } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css'
 import About from "./components/About";
 import Home from "./components/Home";
 import Users from "./components/Users";
 import User from "./components/User";
+import "./components/App.css"
+import { NavLink } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/users">Users</Link>
-          </li>
-        </ul>
+          <ul className="navbar-nav mr-auto list-group-horizontal">
+            <li className="navbar-item active">
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li className="navbar-item">
+              <NavLink to="/about">About</NavLink>
+            </li>
+            <li className="navbar-item">
+              <NavLink to="/users">Users</NavLink>
+            </li>
+          </ul>
       </nav>
+
+    
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/users" element={<Users />} />
         <Route path="/users/:userId" element={<User />} />
-        <Route path="*" element={
-          <main style={{padding: "1rem"}}>
-            <p>WRON PATH!</p>
-          </main>
-
-        }
+        <Route
+          path="*"
+          element={
+            <main style={{ padding: "1rem" }}>
+              <p>WRON PATH!</p>
+            </main>
+          }
         />
-
       </Routes>
     </div>
   );
--- Ogrenciler/Gorkem AYKAC/27.02.2022/router-app/src/components/Home/index.js
+++ Ogrenciler/Gorkem AYKAC/27.02.2022/router-app/src/components/Home/index.js
@@ -1,14 +1,32 @@
 import { Link } from "react-router-dom";
+import styles from "./styles.module.css";
 
 export default function Home() {
   return (
-    <div>
-      <div>
-        <h2>Welcome to the User APP!</h2>
+    <div className={styles.color}>
+      <div className={styles.home}>
+        <div>
+          <h2 className={styles.alignCenter}>Find Random User</h2>
+        </div>
+        <form className={styles.center}>
+          <div className={"form-group"}>
+            <input
+              type="text"
+              className={"form-control "+styles.width}  
+              id="exampleInputName"
+              placeholder="User Name"
+            />
+            <small id="emailHelp" className="form-text text-muted">
+              We'll never share your searching history.
+            </small>
+            <br></br>
+            <button type="submit" className="btn btn-primary">
+            Search
+          </button>
+          </div>
+         
+        </form>
       </div>
-      <nav>
-        <Link to="/about">About</Link>
-      </nav>
     </div>
   );
 }
